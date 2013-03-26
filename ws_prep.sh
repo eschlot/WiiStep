4426f0eeb373bfa0525d77d38ca08f35c087b920
Jack Andersen <jackoalan@gmail.com>
2013-03-26 11:37:31 -0500

    More convenience stuff

---
+++ ws_prep.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Fetch Submodules
+git submodule init
+git submodule update
+
+# Build LLVM (and Clang, compiler-rt)
+pushd llvm
+mkdir build
+pushd build
+cmake -DLLVM_TARGETS_TO_BUILD="PowerPC" ..
+make
+popd
+popd
