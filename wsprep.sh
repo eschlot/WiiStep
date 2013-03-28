f29eb0d427e039a9c82f9af9e12e7ce10f75359a
Jack Andersen <jackoalan@gmail.com>
2013-03-28 16:57:22 -0500

    Much work on packaging WiiStep with cmake and working `make install`

--- wsprep.sh
+++ wsprep.sh
@@ -4,14 +4,5 @@
 git submodule init
 git submodule update
 
-# Build LLVM (and Clang, compiler-rt)
-pushd llvm
-mkdir build
-pushd build
-cmake -DLLVM_TARGETS_TO_BUILD="PowerPC" ..
-make
-popd
-popd
-
 # Note that we finished
 touch wsprep-ran
